4ecf4a238da67f3171bbed297dd34c3357e66c67
meganhodge <mhodge16@uw.edu>
2015-10-11 00:45:19 -0700

    Initial Commit

---
+++ Homework3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework3
+//
+//  Created by Megan Hodge on 10/11/15.
+//  Copyright © 2015 Megan Hodge. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
