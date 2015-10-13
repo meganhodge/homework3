7a32ab8fb4a3deae9e5bb3ee902f7e7a11ee693c
meganhodge <mhodge16@uw.edu>
2015-10-12 22:50:41 -0700

    need to check non-two-arity tuples and nil cases

--- Homework3/main.swift
+++ Homework3/main.swift
@@ -45,8 +45,10 @@ func division(firstDigit: Int?, secondDigit: Int?) -> Int? {
     }
 }
 
-// takes in a math operation function and completes it using the two passed in numbers and returns the result
-func doubleOperation(operation : (Int?, Int?) -> Int?, firstDigit: Int?, secondDigit: Int?) -> Int? {
+// takes in a math operation function and completes it using the two passed 
+// in numbers and returns the result
+func doubleOperation(operation : (Int?, Int?) -> Int?, firstDigit: Int?,
+    secondDigit: Int?) -> Int? {
     if firstDigit == nil || secondDigit == nil {
         return nil
     } else {
@@ -65,11 +67,10 @@ func arrayAddition(numbers: [Int]?) -> Int? {
 
 // takes an array of integers and multiplies them together and returns the total
 func arrayMultiplication(numbers: [Int]?) -> Int? {
-    var total = 1
     if numbers == nil {
         return nil
     } else {
-        return numbers!.reduce(1, combine: +) / numbers!.count
+        return numbers!.reduce(1, combine: *)
     }
 }
 
@@ -83,6 +84,7 @@ func arrayCount(numbers: [Int]?) -> Int? {
 }
 
 // returns the average of the numbers in the array passed in as a parameter
+// rounds to an int so the average of 1 and 2 is 1
 func arrayAvg(numbers: [Int]?) -> Int? {
     if numbers == nil {
         return nil
@@ -110,7 +112,7 @@ func addPoints(pointA: (x: Int?, y: Int?)?, pointB: (x: Int?, y: Int?)?) -> (Int
     }
 }
 
-// subtract the x and y coordinates of two points together to return a third point made up of their sums
+// subtracts the x and y coordinates of two points together to return a third point made up of their sums
 func subtractPoints(pointA: (x: Int?, y: Int?)?, pointB: (x: Int?, y: Int?)?) -> (Int, Int)? {
     if pointA != nil && pointB != nil && pointA!.x != nil && pointB!.x != nil
         && pointA!.y != nil && pointB!.y != nil {
@@ -120,7 +122,50 @@ func subtractPoints(pointA: (x: Int?, y: Int?)?, pointB: (x: Int?, y: Int?)?) ->
     }
 }
 
-// using dictionaries to represent points, add the x and y coordinates of two points to return a third point
-func addPointsDictionary(pointA: [x: Int, y: Int], pointB: [x: Int, y: Int]) -> (Int, Int){
-    
+// using dictionaries to represent points, adds the x and y coordinates of two points to return a third point
+func addPoints(pointA: [String: Double]?, pointB: [String: Double]?) -> (Double, Double)?{
+    if pointA != nil && pointB != nil {
+        if pointA!.keys.contains("x") && pointA!.keys.contains("y") && pointB!.keys.contains("x") && pointB!.keys.contains("y") {
+            return (pointA!["x"]! + pointB!["x"]!, pointA!["y"]! + pointB!["y"]!)
+        }
+    }
+    return (nil)
 }
+
+// using dictionaries to represent points, subtracts the x and y coordinates of two points to return a third point
+func subtractPoints(pointA: [String: Double]?, pointB: [String: Double]?) -> (Double, Double)?{
+    if pointA != nil && pointB != nil {
+        if pointA!.keys.contains("x") && pointA!.keys.contains("y") && pointB!.keys.contains("x") && pointB!.keys.contains("y") {
+            return (pointA!["x"]! - pointB!["x"]!, pointA!["y"]! - pointB!["y"]!)
+        }
+    }
+    return (nil)
+}
+
+// Testing
+print(addition(1,secondDigit: 1)!) // answer: 2
+//print(addition(1, secondDigit: nil)!) // answer: nil
+print(subtraction(1, secondDigit: 1)!) // answer: 0
+print(multiplication(1, secondDigit: 1)!) // answer: 1
+print(division(1, secondDigit: 1)!) // answer: 1
+print(doubleOperation(addition, firstDigit: 1, secondDigit: 1)!) // answer: 2
+print(doubleOperation(subtraction, firstDigit: 1, secondDigit: 1)!) // answer: 0
+print(doubleOperation(multiplication, firstDigit: 1, secondDigit: 1)!) // answer: 1
+print(doubleOperation(division, firstDigit: 1, secondDigit: 1)!) // answer: 1
+
+print(arrayAddition([1,2,3])!) // answer: 6
+print(arrayMultiplication([1,2,3])!) // answer: 6
+print(arrayCount([1,2,3])!) // answer: 3
+print(arrayAvg([1,2, 3])!) // answer: 2
+print(arrayOperations(arrayAddition, firstArray: [1,2,3])!) // answer: 6
+print(arrayOperations(arrayMultiplication, firstArray: [1,2,3])!) // answer: 6
+print(arrayOperations(arrayCount, firstArray: [1,2,3])!) // answer: 3
+print(arrayOperations(arrayAvg, firstArray: [1,2,3])!) // answer: 2
+
+print(addPoints((1,1),pointB: (2,2))!) // answer: (3,3)
+print(subtractPoints((2,2), pointB: (1,1))!) // answer: (1,1)
+
+let pointA = ["z": 1.0, "y": 1.0]
+let pointB = ["x": 1.0, "y": 1.0]
+print(addPoints(pointA, pointB: pointB)!) // answer: (2.0, 2.0)
+print(subtractPoints(pointA, pointB: pointB)!) // answer: (0.0, 0.0)
